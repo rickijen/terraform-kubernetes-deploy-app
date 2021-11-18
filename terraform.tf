a89632310e8d767f892486f581a48bbd2aaf3d79
Rick Jen <rijen@microsoft.com>
2021-11-18 15:12:26 -0800

    fixed references

--- terraform.tf
+++ terraform.tf
@@ -61,7 +61,7 @@ resource "kubernetes_deployment" "color" {
     metadata {
         name = "color-${var.color}"
         labels = {
-            app   = "color"
+            app   = var.app
             color = var.color
         } //labels
     } //metadata
@@ -69,7 +69,7 @@ resource "kubernetes_deployment" "color" {
     spec {
         selector {
             match_labels = {
-                app   = kubernetes_deployment.color.metadata[0].labels.app
+                app   = var.app
                 color = var.color
             } //match_labels
         } //selector
@@ -81,7 +81,7 @@ resource "kubernetes_deployment" "color" {
         template { 
             metadata {
                 labels = {
-                    app   = kubernetes_deployment.color.spec.0.selector.0.match_labels.app
+                    app   = var.app
                     color = var.color
                 } //labels
             } //metadata
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+variable "app" {
+  description = "App name"
+}
 variable "color" {
   description = "The value for the container environment variable COLOR"
 }
