e9ae4b40787322b64b8c089d3c66cca1dad11e26
Rick Jen <rijen@microsoft.com>
2021-11-20 14:58:37 -0800

    updated path

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -11,7 +11,7 @@ resource "kubernetes_ingress" "ingress" {
     rule {
       http {
         path {
-          path = "/${var.k8s_ingress_name}/*"
+          path = "/${var.k8s_ingress_name}"
           backend {
             service_name = var.k8s_ingress_name
             service_port = var.service_port
